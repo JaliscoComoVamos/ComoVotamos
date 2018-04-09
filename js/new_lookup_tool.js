dc6095b79fd03a6de95e7b49f825e4a68ed8f659
Vicki Foss <vfoss@users.noreply.github.com>
2018-04-09 18:46:20 -0500

    some design improvements

--- js/new_lookup_tool.js
+++ js/new_lookup_tool.js
@@ -1,10 +1,6 @@
 var geocoder = new google.maps.Geocoder;
 var marker;
 var loc=[];
-// var INFO_API = 'http://maps.googleapis.com/maps/api/geocode/json';
-// // var MyApp = {};
-
-
 
 function codeAddress() {
     var address = document.getElementById("address").value;
@@ -20,97 +16,7 @@ function codeAddress() {
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
-
-
 }
-
-function display( long_lat ){
-   // document.getElementById('output').innerHTML = long_lat;
-    if (marker){ marker.remove();}
-    marker = new mapboxgl.Marker()
-        .setLngLat([long_lat[1],long_lat[0]])
-        .addTo(map);
-        map.flyTo({center: [long_lat[1],long_lat[0]], zoom: 14, speed: 10});
-    // map.center = [long_lat[1],long_lat[0]];
-}
-
-
-// function addressSearch() {
-
-//     // configuration for showing representatives at different levels of government
-
-//     var show_alcalde = false;
-//     var show_dl = false;
-//     var show_df = false;
-//     var show_senador = false;
-//     var show_gobernador = false;
-//     var show_presidente = false;
-
-
-//     // var results_level_set = [];
-//     // // set levels from checkboxes
-//     // if ($('#show_alcalde_results').is(':checked')) {
-//     //     show_alcalde = true;
-//     //     results_level_set.push('Alcalde');
-//     // }
-//     // if ($('#show_dl_results').is(':checked')) {
-//     //     show_dl = true;
-//     //     results_level_set.push('Diputado Local');
-//     // }
-//     // if ($('#show_df_results').is(':checked')) {
-//     //     show_df = true;
-//     //     results_level_set.push('Diputado Federal');
-//     // }
-//     // if ($('#show_senador_results').is(':checked')) {
-//     //     show_senador = true;
-//     //     results_level_set.push('Senador');
-//     // }
-//     // if ($('#show_gobernador_results').is(':checked')) {
-//     //     show_gobernador = true;
-//     //     results_level_set.push('Gobernador');
-//     // }
-//     // if ($('#show_presidente_results').is(':checked')) {
-//     //     show_presidente = true;
-//     //     results_level_set.push('Presidente');
-//     // }
-
-
-//     var address = $('#address').val();
-
-
-//     geocoder.geocode( { 'address': address}, function(results, status) {
-
-//         if (status == google.maps.GeocoderStatus.OK) {
-//                 var latitude = results[0].geometry.location.lat();
-//                 var longitude = results[0].geometry.location.lng();
-//         } 
-//       }); 
-
-
-//         // $('table tbody').empty();
-
-//         // selected_alcalde = '';
-//         // selected_dl = '';
-//         // selected_df = '';
-//         // selected_senador = '';
-//         // selected_gobernador = '';
-//         // selected_presidente = '';
-//         // all_people = {};
-//         // pseudo_id = 1;
-
-//         // var alcalde_people = [];
-//         // var dl_people = [];
-//         // var df_people = [];
-//         // var senador_people = [];
-//         // var gobernador_people = [];
-//         // var presidente_people = [];
-//         // var federal_people = [];
-//         // var state_people = [];
-//         // var local_people = [];
-//         // var county_people = [];
-
-    
-// };
 
 function findMe() {
     var foundLocation;
@@ -121,8 +27,6 @@ function findMe() {
 
             var accuracy = position.coords.accuracy;
             var coords = new google.maps.LatLng(loc[0], loc[1]);
-
-            // console.log(coords);
 
             geocoder.geocode({
                 'location': coords
@@ -144,3 +48,73 @@ function findMe() {
         alert("La API de geolocalización no es compatible con tu navegador.");
     }
 }
+
+function display( long_lat ){
+    if (marker){ marker.remove();}
+    marker = new mapboxgl.Marker()
+        .setLngLat([long_lat[1],long_lat[0]])
+        .addTo(map);
+        map.flyTo({center: [long_lat[1],long_lat[0]], zoom: 14, speed: 10});
+    addressSearch()
+}
+
+
+function addressSearch() {
+
+    // configuration for showing candidates at different levels of government
+
+    var show_alcalde = false;
+    var show_dl = false;
+    var show_df = false;
+    var show_senador = false;
+    var show_gobernador = false;
+    var show_presidente = false;
+
+
+    var results_level_set = [];
+    // set levels from checkboxes
+    if ($('#show_alcalde_results').is(':checked')) {
+        show_alcalde = true;
+        results_level_set.push('Alcalde');
+    }
+    if ($('#show_dl_results').is(':checked')) {
+        show_dl = true;
+        results_level_set.push('Diputado Local');
+    }
+    if ($('#show_df_results').is(':checked')) {
+        show_df = true;
+        results_level_set.push('Diputado Federal');
+    }
+    if ($('#show_senador_results').is(':checked')) {
+        show_senador = true;
+        results_level_set.push('Senador');
+    }
+    if ($('#show_gobernador_results').is(':checked')) {
+        show_gobernador = true;
+        results_level_set.push('Gobernador');
+    }
+    if ($('#show_presidente_results').is(':checked')) {
+        show_presidente = true;
+        results_level_set.push('Presidente');
+    }
+
+
+        $('table tbody').empty();
+
+        selected_alcalde = '';
+        selected_dl = '';
+        selected_df = '';
+        selected_senador = '';
+        selected_gobernador = '';
+        selected_presidente = '';
+        all_people = {};
+        pseudo_id = 1;
+
+        var alcalde_people = [];
+        var dl_people = [];
+        var df_people = [];
+        var senador_people = [];
+        var gobernador_people = [];
+        var presidente_people = [];
+    
+};
