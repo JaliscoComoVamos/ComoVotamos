f95306ed6cb57b24e416accd3e70919eea876169
Vicki Foss <vfoss@users.noreply.github.com>
2018-04-12 20:40:59 -0500

    finally working (mostly)

--- js/new_lookup_tool.js
+++ js/new_lookup_tool.js
@@ -1,88 +1,3 @@
-var geocoder = new google.maps.Geocoder;
-var marker;
-var loc = [];
-
-function codeAddress() {
-    var address = document.getElementById("address").value;
-    geocoder.geocode({
-        'address': address
-    }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            loc[0] = results[0].geometry.location.lat();
-            loc[1] = results[0].geometry.location.lng();
-
-            display(loc);
-
-        } else {
-            alert("La geocodificación no tuvo éxito por la siguiente razón: " + status);
-        }
-    });
-}
-
-function findMe() {
-    var foundLocation;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            loc[0] = position.coords.latitude;
-            loc[1] = position.coords.longitude;
-
-            var accuracy = position.coords.accuracy;
-            var coords = new google.maps.LatLng(loc[0], loc[1]);
-
-            geocoder.geocode({
-                'location': coords
-            }, function(results, status) {
-                if (status === google.maps.GeocoderStatus.OK) {
-                    if (results[1]) {
-                        $("#address").val(results[1].formatted_address);
-                        display(loc);
-                    }
-                }
-            });
-
-        }, function error(msg) {
-            alert('Permite que el sitio use tu ubicación.');
-        }, {
-            enableHighAccuracy: true
-        });
-    } else {
-        alert("La geolocalización no es compatible con tu navegador.");
-    }
-}
-
-function display(loc) {
-    if (marker) {
-        marker.remove();
-    }
-    marker = new mapboxgl.Marker()
-        .setLngLat([loc[1], loc[0]])
-        .addTo(map)
-
-    map.flyTo({
-        center: [loc[1], loc[0]],
-        zoom: 14,
-        speed: 10
-    });
-    showResults();
-
-    for (i = 0; i < 20; i++) { 
-        map.on('click', 'Distrito ' + i, function(e) {
-                mi_distrito = i
-                console.log(mi_distrito)
-        });
-    }
-
-
-    // map.on('click', 'Distrito 2', function(e) {
-    //             var mi_distrito = 2
-    //             console.log(mi_distrito)
-    //     });
-
-    // map.fire({ lngLat: [loc[1], loc[0]]});
-    // document.getElementById("mi_distrito").innerHTML = mi_distrito;
-
-}
-
 var selected_alcalde = '';
 var selected_dl = '';
 var selected_df = '';
@@ -91,7 +6,6 @@ var selected_gobernador = '';
 var selected_presidente = '';
 var all_people = {};
 var pseudo_id = 1;
-
 
 function showResults() {
 
@@ -137,8 +51,6 @@ function showResults() {
         show_presidente = true;
         results_level_set.push('presidente');
     }
-    
-    // $('table tbody').empty();
 
     selected_alcalde = '';
     selected_dl = '';
@@ -149,59 +61,9 @@ function showResults() {
     all_people = {};
     pseudo_id = 1;
 
-    var info = {'person': null};
-
-    // // Get presidential candidates
-    // $.getJSON("lista_candidatos.json", function(data) {
-    //     for (i = 0; i < data.length; i++) {
-    //         if (data[i].Candidatura === "Presidente") {
-    //             var person = {};
-    //             info['person'] = person
-    //             info.person.person = data[i].Nombre;
-    //             info.person.party = data[i].Partido;
-    //             presidente_people.push(info.person);
-                // all_people[pseudo_id] = info.person;
-                // pseudo_id = pseudo_id + 1;
-                // var result = '';
-                // for(var i=0; i<myArray.length; i++) {
-                //     result += "<tr>";
-                //     for(var j=0; j<myArray[i].length; j++){
-                //         result += "<td>"+myArray[i][j]+"</td>";
-                //     }
-                //     result += "</tr>";
-                // $('#presidente-results tbody').append()
-
-
-
-    // for (i = 0; i < stock.length; i++) {
-    //     var tr = document.createElement('TR');
-    //     for (j = 0; j < stock[i].length; j++) {
-    //         var td = document.createElement('TD')
-    //         td.appendChild(document.createTextNode(stock[i][j]));
-    //         tr.appendChild(td)
-    //     }
-    //     tableBody.appendChild(tr);
-    // }  
-    // myTableDiv.appendChild(table)
-
-
-        //     }
-        // }
-//     });
-// // 
-//     console.log(all_people)
-
-
-    // var template = new EJS({
-    //     'text': $('#tableGuts').html()
-    // });
-
     if (show_alcalde) {
         $('#alcalde-container').show();
         $('#alcalde-nav').show();
-        // $('#alcalde-results tbody').append(template.render({
-        //     people: alcalde_people
-        // }));
     } 
     else {
         $('#alcalde-container').hide()
@@ -211,9 +73,6 @@ function showResults() {
     if (show_dl) {
         $('#dl-container').show();
         $('#dl-nav').show();
-        // $('#dl-results tbody').append(template.render({
-        //     people: dl_people
-        // }));
     } 
     else {
         $('#dl-container').hide()
@@ -223,9 +82,6 @@ function showResults() {
     if (show_df) {
         $('#df-container').show();
         $('#df-nav').show();
-        // $('#df-results tbody').append(template.render({
-        //     people: df_people
-        // }));
     } 
     else {
         $('#df-container').hide()
@@ -235,9 +91,6 @@ function showResults() {
     if (show_senador) {
         $('#senador-container').show();
         $('#senador-nav').show();
-        // $('#senador-results tbody').append(template.render({
-        //     people: senador_people
-        // }));
     } 
     else {
         $('#senador-container').hide()
@@ -247,9 +100,6 @@ function showResults() {
     if (show_gobernador) {
         $('#gobernador-container').show();
         $('#gobernador-nav').show();
-        // $('#gobernador-results tbody').append(template.render({
-        //     people: gobernador_people
-        // }));
     } 
     else {
         $('#gobernador-container').hide()
@@ -259,29 +109,13 @@ function showResults() {
     if (show_presidente) {
         $('#presidente-container').show();
         $('#presidente-nav').show();
-        // $('#presidente-results tbody').append(template.render({
-        //     people: presidente_people
-        // }));
     } 
     else {
         $('#presidente-container').hide()
         $('#presidente-nav').hide();
     }
 
-    // if (marker === undefined || marker === null) {
-    //     $('#no-response-container').show();
-    //     $("#response-container").hide();
-    // } else {
-        $('#no-response-container').hide();
+          $('#no-response-container').hide();
         $("#response-container").show();
-    // }
-
-    // hook up modal stuff
-    // var modal_template = new EJS({'text': $('#modalGuts').html()});
-    // $('.btn-contact').off('click');
-    // $('.btn-contact').on('click', function(){
-    //     var info = all_people[$(this).data('id')];
-    //     $('#modalContent').html(modal_template.render({info: info}));
-    //     $('#contactModal').modal('show');
-    // });
+   
 };
