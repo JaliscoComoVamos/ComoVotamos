e538c573fa27e25f5af66bf3fbb3170665f979b7
jandrajandra <jan.tr9@gmail.com>
2024-02-22 14:33:06 -0600

    Update new_lookup_tool.js

--- js/new_lookup_tool.js
+++ js/new_lookup_tool.js
@@ -124,6 +124,7 @@ function getCandidates() {
                     tableguts = tableguts + "<a href='" + data[i].facebook 
                     + "' target='_blank'><i class='fab fa-facebook'></i></a>"
                 };
+                tableguts = tableguts + </td><td>" + data[i].antecedentes + 
                 items.push(tableguts + "</td></tr>");
             };
         };
